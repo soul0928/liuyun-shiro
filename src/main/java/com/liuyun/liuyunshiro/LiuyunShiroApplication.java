aef70d1aca864338d6a5fe926a9ac4f68c1df71e
soul <soul0928@sina.com>
2019-09-22 12:47:26 +0800

    Initial commit

---
+++ src/main/java/com/liuyun/liuyunshiro/LiuyunShiroApplication.java
@@ -0,0 +1,13 @@
+package com.liuyun.liuyunshiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiuyunShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiuyunShiroApplication.class, args);
+    }
+
+}
